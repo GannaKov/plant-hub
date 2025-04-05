49875e9c2f5f4a0035c53aa423bb10e72035b463
GannaKov <zlatta2000@gmail.com>
2025-04-05 18:52:27 +0200

    added date time

--- components/EquipmentStopForm/EquipmentStopForm.tsx
+++ components/EquipmentStopForm/EquipmentStopForm.tsx
@@ -17,28 +17,43 @@ import {
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 import { Input } from '@/components/ui/input';
 import { toast } from 'sonner';
+import { format } from 'date-fns';
+import { Calendar as CalendarIcon } from 'lucide-react';
+import { Calendar } from '@/components/ui/calendar';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 
 const formSchema = z.object({
   inventory_number: z.string(),
   equipment_name: z.string(),
-  stop_type: z.enum(['a', 'b', 'c', 'd'], {
-    errorMap: () => ({ message: 'Оберіть тип зупинки.' }),
-  }),
-  password: z.string().min(6, {
-    message: 'Пароль повинен містити щонайменше 6 символів.',
-  }),
+  stop_type: z.enum(
+    ['planned-stop', 'service-stop', 'readjustment-stop', 'failure-stop'],
+    {
+      errorMap: () => ({ message: 'Оберіть тип зупинки.' }),
+    }
+  ),
+  date: z.date(),
+  time: z.string(),
 });
 
 const EquipmentStopForm = () => {
+  const [date, setDate] = React.useState<Date>();
+  const [time, setTime] = React.useState<string>('');
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       inventory_number: '',
-      password: '',
+      equipment_name: '',
+
+      stop_type: 'failure-stop',
+      // Format the date to 'YYYY-MM-DD' format
+      date: new Date(), // Current date
+      time: new Date().toTimeString().slice(0, 5), // HH:MM
     },
   });
-
-  const [showPassword, setShowPassword] = useState(false);
 
   const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = (
     values: z.infer<typeof formSchema>
@@ -106,7 +121,6 @@ const EquipmentStopForm = () => {
             )}
           />
           {/* Type Stop */}
-
           <FormField
             control={form.control}
             name="stop_type"
@@ -153,38 +167,63 @@ const EquipmentStopForm = () => {
               </FormItem>
             )}
           />
-          {/* Pasword */}
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem className="mb-8">
-                <FormLabel className="capitalize">Пароль</FormLabel>
-                <div className="relative">
+          <FormLabel className="mb-4">Час зупинки</FormLabel>
+          <FormLabel className="mb-4 text-dark-200">Початок зупинки</FormLabel>
+          <div className="mx-auto flex gap-40">
+            {/* Date */}
+            <FormField
+              control={form.control}
+              name="date"
+              render={({ field }) => (
+                <FormItem className="mb-6">
+                  <FormLabel>Дата</FormLabel>
+                  <FormControl>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant="outline"
+                          className="w-[200px] justify-between text-left font-normal"
+                        >
+                          {field.value
+                            ? field.value.toLocaleDateString()
+                            : 'Оберіть дату'}
+                          <CalendarIcon className="mr-2 h-4 w-4" />
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-auto p-0">
+                        <Calendar
+                          mode="single"
+                          selected={field.value}
+                          onSelect={field.onChange}
+                          initialFocus
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* Time */}
+            <FormField
+              control={form.control}
+              name="time"
+              render={({ field }) => (
+                <FormItem className="mb-6">
+                  <FormLabel>Час</FormLabel>
                   <FormControl>
                     <Input
-                      type={showPassword ? 'text' : 'password'}
+                      type="time"
                       {...field}
-                      style={{
-                        //   backgroundColor: 'white',
-                        WebkitBackgroundClip: 'text',
-                        WebkitTextFillColor: 'white',
-                      }}
+                      className="w-[100px]! justify-between"
                     />
                   </FormControl>
-                  <button
-                    type="button"
-                    className="absolute top-1/2 right-2 -translate-y-1/2 transform"
-                    onClick={() => setShowPassword(!showPassword)}
-                  >
-                    {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-                  </button>
-                </div>
-                <FormDescription>Введіть ваш пароль.</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
           <Button type="submit" className="form-btn">
             Увійти
           </Button>
