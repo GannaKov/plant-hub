116a311d3c020247c4dd1b279401410d49c86eaf
GannaKov <zlatta2000@gmail.com>
2025-04-09 00:02:45 +0200

    changed type stopType

--- database/schema.ts
+++ database/schema.ts
@@ -16,6 +16,13 @@ import {
 //   'REJECTED',
 // ]);
 
+export const STOP_TYPE_ENUM = pgEnum('stop_type', [
+  'planned-stop',
+  'service-stop',
+  'readjustment-stop',
+  'failure-stop',
+]);
+
 export const ROLE_ENUM = pgEnum('role', ['USER', 'ADMIN']);
 // after changing the schema, run this command to generate the new types
 //"db:generate": "npx drizzle-kit generate",
@@ -41,7 +48,7 @@ export const equipment = pgTable('equipment', {
   equipmentType: varchar('equipment_type', { length: 255 }).notNull(),
   equipmentName: varchar('equipment_name', { length: 255 }).notNull(),
   description: text('description').notNull(),
-  stopType: varchar('stop_type', { length: 255 }).notNull(),
+  stopType: STOP_TYPE_ENUM('stop_type').notNull(),
   stopDescription: text('stop_description').notNull(),
   stop_Date: date('stop_date').notNull(),
   stopTime: text('stop_time').notNull(), //or time()?
